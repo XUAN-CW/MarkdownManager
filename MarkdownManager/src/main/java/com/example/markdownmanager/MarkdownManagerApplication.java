26677f075257ce4e85a59cfda54dcefcfc95458d
XUAN-CW <49336037+XUAN-CW@users.noreply.github.com>
2021-07-04 13:23:39 +0800

    创建了项目的后端

---
+++ MarkdownManager/src/main/java/com/example/markdownmanager/MarkdownManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.markdownmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkdownManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarkdownManagerApplication.class, args);
+    }
+
+}
---
+++ MarkdownManager/src/test/java/com/example/markdownmanager/MarkdownManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.markdownmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarkdownManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
